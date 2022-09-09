75f1f5ae03ba965846a6e5c02ade8439905940a8
Venkata Pagadala <vdepagadala@gmail.com>
2022-09-09 16:03:42 -0700

    Updated Similarity score to .40 for a test

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ st.subheader('Directions:')
 st.write('- Upload complete crawl \n - Upload a list of 404s in.CSV format (URL column named URL) \n - Would not '
          'recommend with over 10k URLs (very slow) ')
 st.write("Author - [Venkata Pagadala](https://www.linkedin.com/in/venkata-pagadala/)")
-st.write("Team - [Sarak Dahal](https://www.linkedin.com/in/sarakdahal/)")
 
 # Importing the URL CSV files
 url = st.text_input('The URL to Match', placeholder='Enter domain (www.google.com)')
@@ -42,7 +41,7 @@ if file1 is not None and file2 is not None:
     #Polishing and Pruning
     df1["Similarity"] = df1["Similarity"].round(3)
     df1 = df1.sort_values(by='Similarity', ascending=False)
-    index_names = df1.loc[df1['Similarity'] < .857].index
+    index_names = df1.loc[df1['Similarity'] < .40].index
     amt_dropped = len(index_names)
     df1.drop(index_names, inplace=True)
     df1["To"] = ROOTDOMAIN + df1["To"]
